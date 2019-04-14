6896a58f23bd176282182c4f1253c408e1f4b18f
DevOrc <ncharlton002@gmail.com>
2019-04-13 21:03:10 -0400

    Add RMS

---
+++ rms/src/main/java/org/inspirerobotics/sumobots/rms/RMS.java
@@ -0,0 +1,13 @@
+package org.inspirerobotics.sumobots.rms;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class RMS {
+
+    private static final Logger logger = LogManager.getLogger(RMS.class);
+
+    public static void main(String[] args){
+        logger.info("Started RMS!");
+    }
+}
---
+++ rms/src/test/java/org/inspirerobotics/sumobots/rms/BasicRMSTest.java
@@ -0,0 +1,12 @@
+package org.inspirerobotics.sumobots.rms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BasicRMSTest {
+
+    @Test
+    void basicTest() {
+        Assertions.assertTrue(true);
+    }
+}
